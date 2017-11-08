d2af578f8614e7d1b29a521959d4b4f3b979e373
Adrienne Yao <adrienne.yao@icloud.com>
2017-11-08 20:28:52 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -2,16 +2,16 @@
 #definition of prime number:
 #an integer greater than one, if its only positive divsors are one and itself
 def prime?(integer)
-
+  primeFlag = true
   if [2,3].include?(integer)
       return true
   elsif integer <= 0 || integer == 1
       return false
   else
       (2..(integer-1)).each do |denom|
-        break if integer % denom == 0
+        primeFlag = false if integer % denom == 0 
       end
-      return false
+      
   end
 
   return true
